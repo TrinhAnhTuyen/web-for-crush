10f106f71ae4a89ef948b79ed2f8c1f5d6549754
TrinhAnhTuyen <106819928+TrinhAnhTuyen@users.noreply.github.com>
2022-06-03 22:48:21 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -1,17 +1,17 @@
 const textConfig = {
-  text1: "He luu cậu!",
-  text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.",
-  text3: "Cậu yêu tớ có phải không nào ._.",
-  text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
+  text1: "Hii Vợ tương lai!",
+  text2: "Tớ có điều này muốn nói hỏi cậu.",
+  text3: "Trang yêu Tuyên có phải không nào ",
+  text4: "Nếu cậu không trả lời mà thoát ra tức là muốn làm vợ tớ đó nha -.-",
   text5: "Cậu mơ à???",
-  text6: "Yêu ơi là yêu <3",
-  text7: "lí do cậu thích tớ đi :vvvv",
+  text6: "Yêu ơi là yêuu <3",
+  text7: " Nói lí do cậu thích tớ đi hihi",
   text8: "Gửi cho tớ <3",
-  text9: "Vì cậu đẹp try vlllll",
-  text10: "Tớ biết mà ^^ Yêu cậu 300.000",
+  text9: "Vì cậu đẹp chai vll",
+  text10: "Tớ biết mà ^^ Yêu cậu 3.000",
   text11:
-    "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
-  text12: "Okii lunn <3",
+    "Còn chờ gì nữa mà không nhắn tin cho tớ đi nàoo",
+  text12: "Okii luônn <3",
 };
 
 $(document).ready(function () {
